7af9869af92b41c0021814d303b749533898dbed
Try Ajitiono <ballinst@gmail.com>
2020-05-02 15:46:10 +0700

    add date fns
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export function test() {}
+// Source: https://www.esrl.noaa.gov/gmd/grad/solcalc/solareqns.PDF.
+export function calculate(dayOfYear: number, hour: number) {
+  const fractionalYear =
+    ((2 * Math.PI) / 365) * (dayOfYear - 1 + (hour - 12) / 24);
+}

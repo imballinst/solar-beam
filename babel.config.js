109fd5f69552b77e3b189312cc4809937c10f3f7
Try Ajitiono <ballinst@gmail.com>
2020-05-10 14:01:17 +0700

    ci: update .travis.yml scripts and environment variables
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }, { modules: false }],
-    '@babel/preset-typescript'
-  ]
+  env: {
+    dev: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' }, modules: false }],
+        '@babel/preset-typescript'
+      ]
+    },
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript'
+      ]
+    }
+  }
 };

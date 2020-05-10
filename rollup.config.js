7d72da80efbce509a635ec390d43d73a0b5b1ab2
Try Ajitiono <ballinst@gmail.com>
2020-05-10 13:28:13 +0700

    add terser
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,23 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
-  output: {
-    name: 'solarBeam',
-    file: 'dist/index.js',
-    format: 'umd'
-  },
+  output: [
+    {
+      name: 'solarBeam',
+      file: 'dist/index.js',
+      format: 'umd'
+    },
+    {
+      name: 'solarBeam',
+      file: 'dist/index.min.js',
+      format: 'umd',
+      plugins: [terser()]
+    }
+  ],
   plugins: [
     resolve(),
     babel({
